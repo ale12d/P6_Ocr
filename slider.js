fd04a1251bb05a06e0a69fb8984eeb9d3a2c7f1e
Alexy Dubois <alexy.dubois62@gmail.com>
2022-04-06 18:52:39 +0200

    bug refresh slide

--- slider.js
+++ slider.js
@@ -5,113 +5,79 @@ const api_horror_url = "http://localhost:8000/api/v1/titles/?genre=Horror&sort_b
 
 
 let dic_url = [api_films_url, api_action_url, api_fantasy_url, api_horror_url];
-let dic_slide = [1, 1, 1, 1]
-
-async function showSlides(dic_slide, api_url, i) {
-
-    const response = await fetch(api_url);
-    const data = await response.json();
-    const { results } = data;
-
-    let img_element = document.createElement("img");
-    let img_elementpre = document.createElement("img");
-    let img_elementnext = document.createElement("img");
-    let img_elementpre2 = document.createElement("img");
-    let img_elementnext2 = document.createElement("img");
-
-    let title_element = document.createElement('div');
-    
-    img_element.src = results[dic_slide[i]-1].image_url;
-    img_element.id = "movie";
-    img_elementpre.id ="moviepre";
-    img_elementnext.id = "movienext";
-    img_elementpre2.id ="moviepre+2";
-    img_elementnext2.id = "movienext+2";
+let slide_index = [0, 0, 0, 0]
 
 
-    if (dic_slide[i]-3 == -1){
-        img_elementpre2.src = results[results.length-1].image_url;
-    }
-    else if (dic_slide[i]-3 == -2){
-        img_elementpre2.src = results[results.length-2].image_url;
-    }
-    else {
-        img_elementpre2.src = results[dic_slide[i]-3].image_url;
-    }
+async function setImgs(nb_slide){
 
+    for (i = 0; i < 4;i++){
 
-    if ( dic_slide[i]-2 < 0 ) {
-        img_elementpre.src = results[results.length-1].image_url;
-    }
+        const response = await fetch(dic_url[i]);
+        const data = await response.json();
+        const { results } = data;
 
-    else {
-        img_elementpre.src = results[dic_slide[i]-2].image_url;
+        for (n = 0; n < 25;n++ ){
 
-    }
+            this["img_element"+n] = document.createElement("img");
+            this["img_element"+n].src = results[slide_index[i] + n].image_url;
+            document.getElementsByClassName(`showSlide${i+1}`)[slide_index[i]].appendChild(this["img_element"+n]);
+            if((n  in [0,1,2,3,4]) == false){
+                this["img_element"+n].style.display = "none";
+            }
 
+        }
 
-
-    if ( dic_slide[i] > results.length-1 ) {
-        img_elementnext.src = results[0].image_url;
-    }
-    else {
-        img_elementnext.src = results[dic_slide[i]].image_url;
-    }
-
-
-
-    if (dic_slide[i] == results.length-1){
-        img_elementnext2.src = results[0].image_url;
-    }
-    else if (dic_slide[i] == results.length){
-        img_elementnext2.src = results[1].image_url;
-    }
-    else {
-        img_elementnext2.src = results[dic_slide[i]+1].image_url;
-    }
-    
-    title_element.textContent = "(" + results[dic_slide[i]-1].title + ")";
-    title_element.id = "title-best-movie";
-    let div = document.getElementsByClassName(`showSlide${i+1}`);
-
-    div[dic_slide[i]-1].appendChild(img_elementpre2);
-    div[dic_slide[i]-1].appendChild(img_elementpre);
-    div[dic_slide[i]-1].appendChild(img_element);
-    div[dic_slide[i]-1].appendChild(img_elementnext);
-    div[dic_slide[i]-1].appendChild(img_elementnext2);
-    div[dic_slide[i]-1].appendChild(title_element);
 }
 
-for (let i = 0; i < 4;i++){
-    showSlides(dic_slide, dic_url[i], i);
-    displaySlides(dic_slide, i); 
 }
+setImgs()
 
 function nextSlide(cursorSlide) { 
-    displaySlides(dic_slide[0] += cursorSlide, 0);
-    showSlides(dic_slide, api_films_url, 0);
+    displaySlides(slide_index[0] += cursorSlide, 0);
 }  
 function nextSlide2(cursorSlide2) { 
-    displaySlides(dic_slide[1] += cursorSlide2, 1);
-    showSlides(dic_slide, api_action_url, 1);
+    displaySlides(slide_index[1] += cursorSlide2, 1);
 }  
 function nextSlide3(cursorSlide3) { 
-    displaySlides(dic_slide[2] += cursorSlide3, 2);
-    showSlides(dic_slide, api_fantasy_url, 2);
+    displaySlides(slide_index[2] += cursorSlide3, 2);
 }  
 function nextSlide4(cursorSlide4) { 
-    displaySlides(dic_slide[3] += cursorSlide4, 3);
-    showSlides(dic_slide, api_horror_url, 3);
+    displaySlides(slide_index[3] += cursorSlide4, 3);
 }  
-function displaySlides(cursorSlide, i) {  
 
-    let slides = document.getElementsByClassName(`showSlide${i+1}`);  
 
-    console.log(slides)
-    if (cursorSlide > slides.length) { dic_slide[i] = 1 }  
-    if (cursorSlide < 1) { dic_slide[i] = slides.length }  
-    for (i = 0; i < slides.length; i++) {  
-        slides[i].style.display = "none";  
+function displaySlides(cursorSlide, nb_slide) {  
+
+    console.log("-----");
+
+    let slides = document.getElementsByClassName(`showSlide${nb_slide+1}`);  
+    let all_img = slides[0].getElementsByTagName('img')
+
+    if (cursorSlide > all_img.length) { slide_index[nb_slide] = 0 }  
+    if (cursorSlide < 1) { slide_index[nb_slide] = all_img.length-1 }  
+    console.log({slide_index:slide_index[nb_slide]})
+
+
+    for (i = 0; i < all_img.length; i++) {  
+        all_img[i].style.display = "none";  
     }  
-    slides[dic_slide[i] - 1].style.display = "block";  
+
+    for (i = 0; i < 5;i++){
+        console.log("-----");
+
+        if (slide_index[nb_slide]  + i > 24){
+
+
+            all_img[slide_index[nb_slide] + i - 25].style.display = "inline";  
+            console.log({all_img_index : slide_index[nb_slide]  + i -25});
+        }
+
+        else{
+            all_img[slide_index[nb_slide] +i].style.display = "inline";  
+            console.log({all_img_index : slide_index[nb_slide]  + i});
+        }
+        console.log("-----");
+    }
+    console.log("-----");
+
 }
